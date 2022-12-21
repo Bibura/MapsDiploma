40c814c672750d6615abc8abdb2a91b82b40ee5d
Bibura <programmister@ukr.net>
2022-12-21 14:13:04 +0200

    Added *not* an improvement to routing

--- Maps/Form1.cs
+++ Maps/Form1.cs
@@ -6,6 +6,7 @@ using GMap.NET.WindowsForms.ToolTips;
 using Itinero;
 using Itinero.IO.Osm;
 using Itinero.Osm.Vehicles;
+using Itinero.Profiles;
 using Npgsql;
 using System.Collections;
 using System.Collections.Generic;
@@ -28,19 +29,19 @@ namespace Maps
 
         DBController dBController;
         List<string> searchHistory;
-            DataTable dtRouter = new DataTable();
+        DataTable dtRouter = new DataTable();
 
         public Form1()
         {
             InitializeComponent();
             StartPosition = FormStartPosition.CenterScreen;
-            dBController = new DBController("postgres", "mapsdiploma", "admin");
-            dBController.openConnection();
-            searchHistory = dBController.searchSelect("searchhistory");
-            if (searchHistory != null)
-                foreach (string item in searchHistory)
-                    comboBox1.Items.Add(item);
-            dBController.closeConnection();
+            //dBController = new DBController("postgres", "mapsdiploma", "admin");
+            //dBController.openConnection();
+            //searchHistory = dBController.searchSelect("searchhistory");
+            //if (searchHistory != null)
+            //    foreach (string item in searchHistory)
+            //        comboBox1.Items.Add(item);
+            //dBController.closeConnection();
         }
 
         private void gMapControl1_Load(object sender, EventArgs e)
@@ -55,20 +56,20 @@ namespace Maps
                 GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.CacheOnly; //downloading mode - online of ofline from package
                 gMapControl1.MapProvider = OpenStreetMapProvider.Instance; //provider choose
             }
-                gMapControl1.MinZoom = 2; //min zoom
-                gMapControl1.MaxZoom = 32; //max zoom
-                gMapControl1.Zoom = 10; //opening zoom
-                                        //gMapControl1.Position = new GMap.NET.PointLatLng(50.45, 30.524167);
-                gMapControl1.SetPositionByKeywords("Kyiv, Ukraine");//center point on load
-                gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionWithoutCenter; //zooming mode
-                gMapControl1.CanDragMap = true; //dragging on/off
-                gMapControl1.DragButton = MouseButtons.Left; //dragging button
-                gMapControl1.ShowCenter = false; //show red cross in center
-                gMapControl1.ShowTileGridLines = false; //show or not tile grid
-                gMapControl1.Bearing = 0;
-                gMapControl1.PolygonsEnabled = true;
-                gMapControl1.RoutesEnabled = true;
-                gMapControl1.MarkersEnabled = true;
+            gMapControl1.MinZoom = 2; //min zoom
+            gMapControl1.MaxZoom = 32; //max zoom
+            gMapControl1.Zoom = 10; //opening zoom
+                                    //gMapControl1.Position = new GMap.NET.PointLatLng(50.45, 30.524167);
+            gMapControl1.SetPositionByKeywords("Kyiv, Ukraine");//center point on load
+            gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionWithoutCenter; //zooming mode
+            gMapControl1.CanDragMap = true; //dragging on/off
+            gMapControl1.DragButton = MouseButtons.Left; //dragging button
+            gMapControl1.ShowCenter = false; //show red cross in center
+            gMapControl1.ShowTileGridLines = false; //show or not tile grid
+            gMapControl1.Bearing = 0;
+            gMapControl1.PolygonsEnabled = true;
+            gMapControl1.RoutesEnabled = true;
+            gMapControl1.MarkersEnabled = true;
 
             gMapControl1.Overlays.Add(new GMapOverlay("Temporary markers"));
         }
@@ -187,15 +188,21 @@ namespace Maps
             {
                 var routeDB = RouterDb.Deserialize(stream);
 
-                var profile = Vehicle.Car.Fastest();
+                var profile = Itinero.Osm.Vehicles.Vehicle.Car.Fastest();
                 var router = new Router(routeDB);
 
                 gMapControl1.SetPositionByKeywords(comboBox2.Text);
                 PointLatLng endPoint = new PointLatLng(gMapControl1.Position.Lat, gMapControl1.Position.Lng);
+                GMarkerGoogle destinationPoint = new GMarkerGoogle(endPoint, GMarkerGoogleType.red_big_stop);
+                destinationPoint.ToolTip = new GMapRoundedToolTip(destinationPoint);
+                destinationPoint.ToolTipText = "Destination";
                 var latEnd = Convert.ToSingle(endPoint.Lat);
                 var lngEnd = Convert.ToSingle(endPoint.Lng);
                 gMapControl1.SetPositionByKeywords(comboBox1.Text);
                 PointLatLng startPoint = new PointLatLng(gMapControl1.Position.Lat, gMapControl1.Position.Lng);
+                GMarkerGoogle startingPoint = new GMarkerGoogle(startPoint, GMarkerGoogleType.green_big_go);
+                startingPoint.ToolTip = new GMapRoundedToolTip(startingPoint);
+                startingPoint.ToolTipText = "Start";
                 var latStart = Convert.ToSingle(startPoint.Lat);
                 var lngStart = Convert.ToSingle(startPoint.Lng);
 
@@ -207,7 +214,7 @@ namespace Maps
 
                 List<PointLatLng> points = new List<PointLatLng>();
 
-                foreach(var item in route.Shape)
+                foreach (var item in route.Shape)
                 {
                     PointLatLng tmpPoint = new PointLatLng(Convert.ToDouble(item.Latitude), Convert.ToDouble(item.Longitude));
                     points.Add(tmpPoint);
@@ -216,6 +223,8 @@ namespace Maps
 
                 routOverlay.Routes.Clear();
                 routOverlay.Routes.Add(gMapRoute);
+                routOverlay.Markers.Add(destinationPoint);
+                routOverlay.Markers.Add(startingPoint);
                 gMapControl1.Overlays.Add(routOverlay);
             }
         }
@@ -223,12 +232,12 @@ namespace Maps
         private void downloadOfflineMapToolStripMenuItem_Click(object sender, EventArgs e)
         {
             var routerDb = new RouterDb();
-            using(var stream = new FileInfo(@"ukraine-latest.osm.pbf").OpenRead())
+            using (var stream = new FileInfo(@"ukraine-latest.osm.pbf").OpenRead())
             {
-                routerDb.LoadOsmData(stream, Vehicle.Car);
+                routerDb.LoadOsmData(stream, Itinero.Osm.Vehicles.Vehicle.Car);
             }
 
-            using(var stream = new FileInfo(@"ukraine-latest.routerdb").Open(FileMode.Create))
+            using (var stream = new FileInfo(@"ukraine-latest.routerdb").Open(FileMode.Create))
             {
                 routerDb.Serialize(stream);
             }
@@ -236,8 +245,48 @@ namespace Maps
 
         private void button2_Click(object sender, EventArgs e)
         {
-            gMapControl1.Position = new GMap.NET.PointLatLng(50.43115, 30.657337);
-            
+            gMapControl1.SetPositionByKeywords(comboBox2.Text);
+            PointLatLng endPoint = new PointLatLng(gMapControl1.Position.Lat, gMapControl1.Position.Lng);
+            List<PointLatLng> points = new List<PointLatLng>();
+            GMarkerGoogle destinationPoint = new GMarkerGoogle(endPoint, GMarkerGoogleType.red_big_stop);
+            destinationPoint.ToolTip = new GMapRoundedToolTip(destinationPoint);
+            destinationPoint.ToolTipText = "Destination";
+            gMapControl1.SetPositionByKeywords(comboBox1.Text);
+            PointLatLng startPoint = new PointLatLng(gMapControl1.Position.Lat, gMapControl1.Position.Lng);
+            GMarkerGoogle startingPoint = new GMarkerGoogle(startPoint, GMarkerGoogleType.green_big_go);
+            startingPoint.ToolTip = new GMapRoundedToolTip(startingPoint);
+            startingPoint.ToolTipText = "Start";
+
+            var googleRoute = OpenStreetMapProvider.Instance.GetRoute(startPoint, endPoint, false, false, 14);
+            using (var stream = new FileInfo(@"ukraine-latest.routerdb").Open(FileMode.Open))
+            {
+                var routeDB = RouterDb.Deserialize(stream);
+
+                var profile = Itinero.Osm.Vehicles.Vehicle.Car.Fastest();
+                var router = new Router(routeDB);
+            for(int i = 0; i < googleRoute.Points.Count - 1; i++)
+            {
+
+                var start = router.Resolve(profile, Convert.ToSingle(googleRoute.Points[i].Lat), Convert.ToSingle(googleRoute.Points[i].Lng), 100);
+
+                var end = router.Resolve(profile, Convert.ToSingle(Convert.ToSingle(googleRoute.Points[i+1].Lat)), Convert.ToSingle(googleRoute.Points[i+1].Lng), 100);
+
+                var OSMroute = router.Calculate(profile, start, end);
+
+                foreach(var a in OSMroute.Shape)
+                {
+                    points.Add(new PointLatLng(a.Latitude, a.Longitude));
+                }
+
+            var routeOnMap = new GMapRoute(points, "My route");
+            routOverlay.Routes.Add(routeOnMap);
+            gMapControl1.Overlays.Add(routOverlay);
+
+            }
+
+            }
+            MessageBox.Show(points.Count.ToString());
+            MessageBox.Show(googleRoute.Points.Count.ToString());
         }
     }
 }
